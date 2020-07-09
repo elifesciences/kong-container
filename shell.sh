f7d17b15438f1ce1bfe9ef73c5b330cbff0d4c9d
Luke <lsh-0@users.noreply.github.com>
2020-07-09 17:55:09 +0930

    adds shell.sh helper script

--- shell.sh
+++ shell.sh
@@ -2,11 +2,13 @@
 set -e
 
 docker run \
+    -it \
     --rm \
     --name kong \
     --net host \
     --publish 8000:8000 \
-    elifesciences/kong:${IMAGE_TAG:-latest} 
+    elifesciences/kong:${IMAGE_TAG:-latest} \
+    bash
 
 # stop+remove with:
 # docker rm -f kong
